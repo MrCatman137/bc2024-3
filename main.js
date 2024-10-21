03b3c25011167d4bee2edfcdc7d2ff7600bc43bd
Володимир bc2024-4 <mr.catmanes@gmail.com>
2024-10-21 09:02:30 +0300

    Changed main.js

--- main.js
+++ main.js
@@ -2,23 +2,29 @@ const { program } = require("commander");
 const fs = require("fs");
 const path = require("path");
 
-program.exitOverride();
+//program.exitOverride();
 
 program
-  .requiredOption("-i, --input <path>", "Path to input file")
+  .option("-i, --input <path>", "Path to input file")
   .option("-o, --output <path>", "Path to output file")
-  .option("-d, --display", "Display the result in console")
-  .option("-m, --maxRate", "Display the maximum currency rate");
+  .option("-d, --display", "Display the result in console");
+//  .option("-m, --maxRate", "Display the maximum currency rate");
+program.parse(process.argv);
 
-try {
+/*try {
   program.parse(process.argv);
 } catch (error) {
   if (error.code === "commander.missingMandatoryOptionValue") {
     console.error("Please, specify input file");
     process.exit(1);
   }
-}
+}*/
 const options = program.opts();
+
+if (!options.input) {
+  console.error("Please, specify input file");
+  process.exit(1);
+}
 
 function readFile(filePath) {
   const fullPath = path.resolve(filePath); // converting relative file path to absolute path
@@ -56,20 +62,28 @@ if (maxRate === -Infinity) {
   process.exit(1);
 }
 
-if (options.output && options.display) {
+/*if (options.output && options.display) {
   const outputPath = path.resolve(options.output);
   fs.writeFileSync(outputPath, JSON.stringify(inputData, null, 2), "utf-8");
   console.log("Result is written\nDisplay the result:");
   console.log(JSON.stringify(inputData, null, 2));
-} else if (options.output) {
+} else*/
+if (options.output) {
   const outputPath = path.resolve(options.output);
   fs.writeFileSync(outputPath, JSON.stringify(inputData, null, 2), "utf-8");
   console.log("Result is written");
-} else if (options.display) {
+  const rateOutputPath = path.resolve("maxRateOutput.txt");
+  fs.writeFileSync(rateOutputPath, maxRate.toString(), "utf-8");
+  console.log("Max rate is written");
+}
+
+if (options.display) {
   console.log("Display the result:");
   console.log(JSON.stringify(inputData, null, 2));
-} else if (options.maxRate) {
+  console.log(`Максимальний курс: ${maxRate}`);
+}
+/* else if (options.maxRate) {
   console.log(`Максимальний курс: ${maxRate}`);
 } else {
   process.exit(0);
-}
+}*/
